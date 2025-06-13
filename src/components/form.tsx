eb3cf815a8623cae93536dae39991bcbe9a9835f
Paul Scanlon <pauliescanlon@gmail.com>
2025-06-13 10:31:42 -0700

    test: use React form

---
+++ src/components/form.tsx
@@ -0,0 +1,19 @@
+import { actions } from "astro:actions";
+import { useState } from "react";
+
+export const Form = () => {
+  const [result, setResult] = useState<string | null>(null);
+
+  async function handleSubmit() {
+    const { data } = await actions.getGreeting({ name: "Houston" });
+
+    setResult(data || null);
+  }
+
+  return (
+    <>
+    <div>{result}</div>
+    <button onClick={handleSubmit}>Get greeting</button>
+    </>
+  );
+};
